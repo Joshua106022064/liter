ea0adc767362ad4ee0217630216cc5762f4750b8
unknown <joshua310041@gmail.com>
2017-10-15 22:40:52 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        float x;
+        x=in.nextFloat();
+        System.out.printf("%.1f",x*0.26418);
+        System.out.println();
+
+    }
+}
